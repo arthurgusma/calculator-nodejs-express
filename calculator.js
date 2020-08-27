aa4f8e01568aceb221ae961bb2d9983aa7889e06
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-08-27 16:24:01 -0300

    Update: npm body-parser included to the project.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded())
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html")
