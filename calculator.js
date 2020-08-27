169116503f7d8e8c3dbe8bda1516a52c0c582617
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-08-27 17:00:31 -0300

    New: created a page to calculete the user BMI on the server.

--- calculator.js
+++ calculator.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/index.html")
-})
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.listen(3000)
+app.post("/", function(req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+  res.send("The sum of your inputs is: " + result)
+});
+
+app.get("/bmi", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+});
+
+app.post("/bmi", function(req, res) {
+ var height = Number(req.body.height);
+ var weight = Number(req.body.weight);
+ var result = weight / (height * height);
+ res.send("Your BMI is: " + result);
+});
+
+app.listen(3000);
